d5a63aebabff60e70127e9aa8868c7387ac2c50b
+ <omuhindaaaron@gmail.com>
2024-06-09 20:14:58 +0300

    feat: Adding methods to create authors and retrive their articles and magazines

--- models/author.py
+++ models/author.py
@@ -1,7 +1,62 @@
+from database.connection import get_db_connection
+
 class Author:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        self._create_author()
 
     def __repr__(self):
         return f'<Author {self.name}>'
+    
+    def _create_author(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO authors (id, name) VALUES (?, ?)
+        ''', (self.id, self.name))
+        conn.commit()
+        conn.close()
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        if not isinstance(value, int):
+            raise TypeError("ID must be an integer")
+        self._id = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("Name must be a non-empty string")
+        self._name = value
+
+    def articles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT * FROM articles WHERE author_id = ?
+        ''', (self.id,))
+        articles = cursor.fetchall()
+        conn.close()
+        return articles
+
+    def magazines(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT DISTINCT magazines.* FROM magazines
+            JOIN articles ON magazines.id = articles.magazine_id
+            WHERE articles.author_id = ?
+        ''', (self.id,))
+        magazines = cursor.fetchall()
+        conn.close()
+        return magazines
+
